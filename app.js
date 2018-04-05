01ec1b2f878c688cd44179daca1be95bd08ee005
k.pawelus <k.pawelus@o2.pl>
2018-04-05 04:59:41 +0200

    moved dice variable inside anonymous function in event listener, made dice image visible again but only if event (click on 'roll dice'button is activated)

--- app.js
+++ app.js
@@ -15,14 +15,21 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0; 
 //activePlayer = 1 for second player
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//this is setter because it sets the vaule, getter is a variable in which we store already existing value so we can print it later
 
 document.querySelector('.dice').style.display = 'none';
 //hiding dice at the beginning of the game
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+	//1. Generate random number (we need dice variable here so I took it form up there and declared it here, it will be available only in this anonymous function due to the scoping chain)
+	var dice = Math.floor(Math.random() * 6) + 1;
+	
+	//2. Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+	
+	//3. Update the round score but only IF generated number is not 1 
+	document.querySelector('#current-' + activePlayer).textContent = dice;
+	//this is setter because it sets the vaule, getter is a variable in which we store already existing value so we can print it later
 	
 });
